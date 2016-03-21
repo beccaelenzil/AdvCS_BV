de1daf7101dc1912dc5a9b174e540b180d90298f
Becca Elenzil <beccaelenzil@gmail.com>
2016-03-21 11:10:03 -0700

    -1 and 1s put into connectFour test

--- ConnectFour.py
+++ ConnectFour.py
@@ -124,4 +124,113 @@ class Board:
         print ("O" if turn==1 else "X") + " wins!"
 
 c = Board(7, 6)
-c.hostGame()
+#c.hostGame()
+
+# Connect Four Tests
+
+print "---------------------------------------------"
+print "print a 7 x 6 board with the columns numbered"
+print "---------------------------------------------\n"
+b = Board(7,6)
+print b
+
+print " "
+print "---------------------------------------------"
+print "test addMove"
+print "---------------------------------------------\n"
+print "| | | | | | | |"
+print "| | | | | | | |"
+print "| | | | | | | |"
+print "|X| | | | | | |"
+print "|O| | | | | | |"
+print "|X| | |O|O|O|O|"
+print "---------------"
+print " 0 1 2 3 4 5 6\n"
+print "==\n"
+b.addMove(0, 1)
+b.addMove(0, -1)
+b.addMove(0, 1)
+b.addMove(3, -1)
+b.addMove(4, -1)  # cheating by letting O go again!
+b.addMove(5, -1)
+b.addMove(6, -1)
+print b
+
+print " "
+print "---------------------------------------------"
+print "test clear"
+print "---------------------------------------------\n"
+print "print an empty board"
+b.clear()
+print b
+
+print " "
+print "---------------------------------------------"
+print "test allowsMove"
+print "---------------------------------------------\n"
+b = Board(2,2)
+b.addMove(0, 1)
+b.addMove(0, -1)
+print b
+print " "
+print "b.allowsMove(-1) should be False == ",b.allowsMove(-1)
+print "b.allowsMove(0) should be False == ",b.allowsMove(0)
+print "b.allowsMove(1) should be True == ",b.allowsMove(1)
+print "b.allowsMove(2) should be False == ",b.allowsMove(2)
+
+print " "
+print "---------------------------------------------"
+print "test isFull"
+print "---------------------------------------------\n"
+b = Board(2,2)
+print b
+print " "
+print "b.isFull() should be False == ", b.isFull()
+print " "
+b.setBoard( '0011' )
+print b
+print " "
+print "b.isFull() should be True == ", b.isFull()
+
+
+print " "
+print "---------------------------------------------"
+print "test delMove"
+print "---------------------------------------------\n"
+
+b = Board(2,2)
+b.setBoard( '0011' )
+print b
+print "after the following commands: \n \
+b.delMove(1) \n \
+b.delMove(1) \n \
+b.delMove(1) \n \
+b.delMove(0) \n \
+The board should look like: \n \
+| | | \n \
+|X| | \n \
+-----\n \
+ 0 1 \n \
+ == "
+b.delMove(1)
+b.delMove(1)
+b.delMove(1)
+b.delMove(0)
+print b
+
+print " "
+print "---------------------------------------------"
+print "test winsFor"
+print "---------------------------------------------\n"
+
+
+b = Board(7,6)
+b.setBoard( '00102030' )
+print "if b.setBoard( '00102030' ), then b.winsFor('X') should be True == ",b.winsFor('1')
+print "if b.setBoard( '00102030' ), then b.winsFor('O') should be True == ",b.winsFor('-1')
+
+b = Board(7,6)
+b.setBoard( '23344545515'  )
+print "if b.setBoard( '23344545515'  ), then b.winsFor('X') should be True == ",b.winsFor('1')
+print "if b.setBoard( '23344545515'  ), then b.winsFor('O') should be False == ",b.winsFor('-1')
+
