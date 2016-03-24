a08da043d84cc1b3b99fc0979b17dfe8b6b6489d
lildutchie99 <misterhamin@gmail.com>
2016-03-24 10:58:03 -0700

    more cc but some structural changes so not stable

--- ChineseCheckers/ChineseCheckers.py
+++ ChineseCheckers/ChineseCheckers.py
@@ -33,30 +33,30 @@ class Hole:
             self.shape.color = color.yellow
         else:
             self.shape.color = color.black
-    def empty(self):
-        return self.marble == None
+    def __repr__(self):
+        return "(" + str(self.f) + ", " + str(self.g) + "), " + ("occupied by a " + str(self.marble.sphere.color) + " marble" if self.marble != None else " empty")
 
 class Board:
     def __init__(self, players):
         self.players = players
         self.board = cylinder(pos = (0, 7, -.5), axis = (0, 0, .5), radius = 8, height = .5, material = materials.wood)
-        self.marbles = []
+        self.marbles = [[None] * 12] * 12
         startPts = [(0, 0), (8, -1), (9, 0), (8, 8), (0, 9), (-1, 8)]
-        self.holes = []
+        self.holes = [[None] * 12] * 12
         for p in range(6):
             for f in range(4):
                 for g in range(4 - f):
-                    self.marbles.append(Marble(startPts[p][0] + f * (-1 if p%2==1 else 1), startPts[p][1] + g * (-1 if p%2==1 else 1), p))
-                    self.holes.append(Hole(f, g, self.marbles[-1]))
+                    self.marbles[f][g] = Marble(startPts[p][0] + f * (-1 if p%2==1 else 1), startPts[p][1] + g * (-1 if p%2==1 else 1), p)
+                    self.holes[f][g] = Hole(f, g, self.marbles[f][g])
         self.unitmoves = [(0, 1), (1, 0), (1, -1), (0, -1), (-1, 0), (-1, 1)]
         #make holes
         for startf in range(4):
             loc = (startf, 4)
             for move in self.unitmoves:
                 for n in range(4 - startf):
-                    self.holes.append(Hole(loc[0], loc[1], None))
+                    self.holes[loc[0]][loc[1]] = Hole(loc[0], loc[1], None)
                     loc = (loc[0] + move[0], loc[1] + move[1])
-        self.holes.append(Hole(4, 4, None))
+        self.holes[4][4] = Hole(4, 4, None)
 
     def hostGame(self):
         prevSelectedShape = None
@@ -68,15 +68,31 @@ class Board:
                 if prevSelectedShape != p:
                     if prevSelectedShape != None:
                             prevSelectedShape.color = prevSelectedColor
-                    if p != None and len([self.marbles[x].sphere for x in range(len(self.marbles)) if self.marbles[x].sphere == p]) > 0:
+                    if p != None and len([[self.marbles[f][g] for f in range(len(self.marbles))] for g in range(len(self.marbles[0])) if self.marbles[f][g].sphere == p]) > 0:
                         prevSelectedColor = p.color
                         p.color = color.magenta
                         prevSelectedShape = p
                 sleep(0.1)
-            marble = [self.marbles[x] for x in range(len(self.marbles)) if self.marbles[x].sphere == prevSelectedShape][0]
-            selectables = [self.holes[x] for x in range(len(self.holes)) if (self.holes[x].f-marble.f, self.holes[x].g-marble.g) in self.unitmoves]
+            obj = scene.mouse.pick
+            scene.mouse.getclick()
+            marbles = [self.marbles[x] for x in range(len(self.marbles)) if self.marbles[x].sphere == obj]
+            if len(marbles)==0:
+                continue
+            marble = marbles[0]
+            #see if are within 1 unit in both directions, and also check to see if diff is not same for both b/c (-1,-1), (0,0), and (1, 1) aren't legal
+            selectables = []
+            for x in range(len(self.holes)):
+                if abs(self.holes[x].f-marble.f) <= 1 and abs(self.holes[x].g-marble.g) <= 1 and (self.holes[x].f-marble.f != self.holes[x].g-marble.g): #if is legal move
+                    if self.holes[x].marble == None: #if empty hole
+                        selectables.append(self.holes[x])
+                    else: #can skip?
+                        checkf = self.holes[x].f + (self.holes[x].f-marble.f) #another scoot in the direction
+                        checkg = self.holes[x].g + (self.holes[x].g-marble.g)
+
+
             for s in selectables:
-                s.shape.color = color.green
+                print s
+
 
 
 b = Board(6)
