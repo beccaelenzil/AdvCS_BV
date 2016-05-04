1a33142f423289aa33e1606154f27cf57da3418c
lildutchie99 <misterhamin@gmail.com>
2016-05-04 14:12:15 -0700

    stacks stuff

---
+++ Stacks.py
@@ -0,0 +1,56 @@
+import math
+
+class Stack:
+    def __init__(self):
+        self.array = []
+    def push(self, item):
+        self.array.append(item)
+    def pop(self):
+        item = self.array[-1]
+        del self.array[-1]
+        return item
+    def peek(self):
+        return self.array[-1]
+    def isEmpty(self):
+        return len(self.array) == 0
+    def size(self):
+        return len(self.array)
+
+def parenChecker(parenString):
+    s = Stack()
+    for p in parenString:
+        if p == '(':
+            s.push(p)
+        elif p == ')':
+            if s.isEmpty():
+                return False
+            else:
+                s.pop()
+    return s.isEmpty()
+
+def balancedSymbols(str):
+    s = Stack()
+    for p in str:
+        if p in "{([":
+            s.push(p)
+        else:
+            if s.isEmpty():
+                return False
+            elif ("})]")[("{([").index(s.pop())] != p:
+                return False
+    return s.isEmpty()
+
+def dToB(n):
+    s = Stack()
+    [s.push(str(thing%2)) for thing in [n/(2**x) for x in range(int(math.ceil(math.log(n, 2))))]]
+    return "".join(s.array.__reversed__())
+
+def parenCheckerSansStacks(list):
+    ctr = 0
+    for ch in list:
+        ctr += 1 if ch=='(' else -1
+        if ctr < 0:
+            return False
+    return ctr==0
+
+print dToB(237)
